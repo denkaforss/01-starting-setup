1f12bce3ec248d48268c63ee5120174fd1e0b7e6
denkaforss <dennis.forss@gmail.com>
2022-03-15 14:43:15 +0100

    adjustments
    adjusted to instructor's solution

--- src/App.js
+++ src/App.js
@@ -23,25 +23,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <Expenses
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <Expenses
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <Expenses
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <Expenses
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
+      <Expenses items={expenses}
       />
     </div>
   );
